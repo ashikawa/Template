1930e8392cc97a2c5256c1788dd010d24d0155da
a.shigeru <a.shigeru@gmail.com>
2013-08-02 12:11:50 +0900

    gaタグ アップデート
    ユニバーサル版へ

--- js/src/ga.js
+++ js/src/ga.js
@@ -1,17 +1,7 @@
-/*jslint nomen:true browser: true*/
-(function () {
-    'use strict';
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-    var _gaq = window._gaq || [];
-    _gaq.push(['_setAccount', 'UA-XXXXX-1']);
-    _gaq.push(['_trackPageview']);
-
-    (function () {
-        var ga   = document.createElement('script'),
-            s    = document.getElementsByTagName('script')[0];
-        ga.type  = 'text/javascript';
-        ga.async = true;
-        ga.src   = ('https:' === document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        s.parentNode.insertBefore(ga, s);
-    }());
-}());
+ga('create', 'UA-XXXXXXXX-1', 'example.com');
+ga('send', 'pageview');
