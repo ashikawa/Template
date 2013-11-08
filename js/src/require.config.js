cad466abb2ed530c2d2826b8644ada7282351dd6
a.shigeru <a.shigeru@gmail.com>
2013-11-08 21:02:15 +0900

    js-url から jQuery.qurl に変更

--- js/src/require.config.js
+++ js/src/require.config.js
@@ -11,6 +11,7 @@
             'jquery.cookie': {deps: ['jquery']},
             'jquery.tmpl':   {deps: ['jquery']},
             'jquery.pjax':   {deps: ['jquery']},
+            'jquery.purl':   {deps: ['jquery'], exports: 'url'},
             'bootstrap':     {deps: ['jquery']},
             'underscore': {exports: '_'},
             'backbone': {
@@ -20,7 +21,6 @@
             'backbone.localstrage': {
                 deps: ['backbone']
             },
-            'js-url': {exports: 'url'},
             'less': {}
         },
         paths: {
@@ -29,11 +29,11 @@
             'jquery.cookie': '//cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.3.1/jquery.cookie.min',
             'jquery.tmpl':   '//ajax.microsoft.com/ajax/jquery.templates/beta1/jquery.tmpl.min',
             'jquery.pjax':   '//cdnjs.cloudflare.com/ajax/libs/jquery.pjax/1.7.0/jquery.pjax.min',
+            'jquery.purl':        '//cdnjs.cloudflare.com/ajax/libs/jquery-url-parser/2.3.1/purl.js',
             'bootstrap':     '//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min',
             'underscore':    '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.2/underscore-min',
             'backbone':      '//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.0.0/backbone-min',
             'backbone.localstrage': '//cdnjs.cloudflare.com/ajax/libs/backbone-localstorage.js/1.1.0/backbone.localStorage-min',
-            'js-url':        '//cdnjs.cloudflare.com/ajax/libs/js-url/1.8.4/url.min',
             'less':          '//cdnjs.cloudflare.com/ajax/libs/less.js/1.4.1/less.min'
         }
     });
