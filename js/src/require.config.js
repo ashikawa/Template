1187af0cb83284e8979f1e870ea27fb1a3b9632f
ashikawa <ashikawa@nulab.co.jp>
2015-01-22 15:44:18 +0900

    update js libraries

--- js/src/require.config.js
+++ js/src/require.config.js
@@ -23,19 +23,18 @@
             'moment': {}
         },
         paths: {
-            'jquery':        '//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min',
+            'jquery':        '//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min',
             'jquery.easing': '//cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min',
-            'jquery.cookie': '//cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.3.1/jquery.cookie.min',
-            'jquery.tmpl':   '//ajax.microsoft.com/ajax/jquery.templates/beta1/jquery.tmpl.min',
-            'jquery.pjax':   '//cdnjs.cloudflare.com/ajax/libs/jquery.pjax/1.7.0/jquery.pjax.min',
+            'jquery.cookie': '//cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min',
+            'jquery.pjax':   '//cdnjs.cloudflare.com/ajax/libs/jquery.pjax/1.9.2/jquery.pjax.min',
             'jquery.purl':   '//cdnjs.cloudflare.com/ajax/libs/jquery-url-parser/2.3.1/purl.min',
-            'bootstrap':     '//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min',
-            'underscore':    '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.5.2/underscore-min',
-            'backbone':      '//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.0.0/backbone-min',
-            'backbone.localstrage': '//cdnjs.cloudflare.com/ajax/libs/backbone-localstorage.js/1.1.0/backbone.localStorage-min',
-            'knockout':      '//cdnjs.cloudflare.com/ajax/libs/knockout/3.1.0/knockout-min',
-            'less':          '//cdnjs.cloudflare.com/ajax/libs/less.js/1.4.1/less.min',
-            'moment':        '//cdnjs.cloudflare.com/ajax/libs/moment.js/2.5.1/moment-with-langs'
+            'bootstrap':     '//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.2/js/bootstrap.min',
+            'underscore':    '//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.7.0/underscore-min',
+            'backbone':      '//cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.2/backbone-min',
+            'backbone.localstrage': '//cdnjs.cloudflare.com/ajax/libs/backbone-localstorage.js/1.1.15/backbone.localStorage-min',
+            'knockout':      '//cdnjs.cloudflare.com/ajax/libs/knockout/3.2.0/knockout-min',
+            'less':          '//cdnjs.cloudflare.com/ajax/libs/less.js/2.2.0/less.min',
+            'moment':        '//cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment-with-langs'
         }
     });
 }());
